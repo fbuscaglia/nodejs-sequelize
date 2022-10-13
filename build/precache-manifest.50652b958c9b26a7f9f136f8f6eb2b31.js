caf50ce6f8d49102b0a75c2169793adc7ef3617d
fbuscaglia <fbuscaglia@gctechs.com>
2022-10-13 14:25:32 -0300

    Deploy v1.1

---
+++ build/precache-manifest.50652b958c9b26a7f9f136f8f6eb2b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d19fb4c2780379b5939b",
+    "url": "/static/css/main.cda1fbed.chunk.css"
+  },
+  {
+    "revision": "d19fb4c2780379b5939b",
+    "url": "/static/js/main.0283703f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7fb74adaa96736fca77a",
+    "url": "/static/css/2.f8d4c8f6.chunk.css"
+  },
+  {
+    "revision": "7fb74adaa96736fca77a",
+    "url": "/static/js/2.96df0172.chunk.js"
+  },
+  {
+    "revision": "af59b04fb886dbc461fbe789b92d4b38",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 import { config } from "dotenv";
+import express from "express";
+import path from 'path';
+import {fileURLToPath} from 'url';
 config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Settings
 app.set("port", process.env.PORT || 4000);
@@ -12,5 +18,12 @@ async function main() {
     console.log(`Server on port ${app.get("port")}`);
   });
 }
+
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 main();
